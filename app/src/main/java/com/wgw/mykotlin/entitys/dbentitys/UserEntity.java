237e535cc4996c7569a3200f0cbd695d0aaa056d
wangguowei <wangguowei@jinrongweb.com>
2018-08-17 17:42:39 +0800

    加入GreenDao

--- app/src/main/java/com/wgw/mykotlin/entitys/dbentitys/UserEntity.java
+++ app/src/main/java/com/wgw/mykotlin/entitys/dbentitys/UserEntity.java
@@ -3,6 +3,7 @@ package com.wgw.mykotlin.entitys.dbentitys;
 import org.greenrobot.greendao.annotation.Entity;
 import org.greenrobot.greendao.annotation.Id;
 import org.greenrobot.greendao.annotation.Property;
+import org.greenrobot.greendao.annotation.Generated;
 
 /**
  * @author wgw
@@ -13,13 +14,60 @@ import org.greenrobot.greendao.annotation.Property;
 public class UserEntity {
     @Id
     private Long ID;
+
     @Property(nameInDb="USERNAME")
     String name;
+
     @Property(nameInDb = "USERAGE")
     int age;
+
     @Property(nameInDb = "USERSEX")
     String sex;
+
     @Property
     String password;
+
+    public UserEntity() {
+    }
+
+    public Long getID() {
+        return this.ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return this.sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
